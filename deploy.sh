040f838736ada36075c1d779ed7fc36c4a929f58
Burak Kurt <burak.kurt@udemy.com>
2022-07-07 02:15:25 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 # this is your part to fill
+# Params
+NAMESPACE=prod
+APPS=(invoice-app payment-provider)
+
+# Build images
+
+
+# Create prod namesapce if not exist
+kubectl get ns $NAMESPACE 2> /dev/null
+if [ $? -ne 0 ]; then
+  kubectl create ns $NAMESPACE
+fi
+
+# Deploy applicaitons
+for app in "${APPS[@]}"
+do
+  kubectl apply -f ${app}/deployment.yaml -n $NAMESPACE
+done
